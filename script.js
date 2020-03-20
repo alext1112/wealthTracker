e15d1a8e20f6050cdc07253d11ae6e1a031bbdf5
Alex Travick <alextravick18@gmail.com>
2020-03-20 14:07:30 -0400

    first commit, HTML, CSS, some JS

---
+++ script.js
@@ -0,0 +1,71 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionaireBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+}
+
+// Double everyones money
+function doubleMoney() {
+  data = data.map(user => {
+    return {...user, money: user.money * 2};
+  });
+
+
+  updateDOM();
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+
+  providedData.forEach(item => {
+    // Create new element for each person
+    const element = document.createElement('div');
+    // Add 'person' class to new element
+    element.classList.add('person');
+    // fill it in with html
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    // insert item into DOM
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');  // 12,345.67
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
